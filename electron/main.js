3a51de9a7c8d3544fc3801a0b514c0066fe75210
James <jamesman45@gmail.com>
2025-04-12 11:59:12 -0400

    feat: adding in release updates (#1)
    * feat: adding in release updates

--- electron/main.js
+++ electron/main.js
@@ -1,15 +1,34 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, autoUpdater, dialog } = require('electron');
+const path = require('path');
 
 let mainWindow;
+
+const feedURL = 'https://github.com/gooey-synths/gooey-app/releases/latest/download/update.json';
+
+autoUpdater.setFeedURL({ url: feedURL });
 
 app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      devTools: true
     }
   });
 
-  mainWindow.loadURL('http://localhost:4200');
+  const isProd = process.env.NODE_ENV === 'production' || app.isPackaged;
+
+  if (isProd) {
+    mainWindow.loadURL(`file://${path.join(__dirname, 'dist/gooey-app/browser/index.html')}`);
+  } else {
+    mainWindow.loadURL('http://localhost:4200');
+  }
+
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
