918b376efb903f271be57fbfc0bb73e0187fb2a4
James <jamesman45@gmail.com>
2025-05-23 13:49:03 -0400

    creating a basic layout for the application and some basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './node-selector/node-selector.component';
+import { MainCanvasComponent } from "./main-canvas/main-canvas.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, MainCanvasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/main-canvas/main-canvas.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {
+  FFlowModule,
+} from '@foblex/flow';
+
+@Component({
+  selector: 'app-main-canvas',
+  imports: [FFlowModule],
+  templateUrl: './main-canvas.component.html',
+  styleUrl: './main-canvas.component.scss'
+})
+export class MainCanvasComponent {
+
+}
