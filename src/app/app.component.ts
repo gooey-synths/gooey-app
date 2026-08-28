ae15fcd587ad06426f19dc245e99c96232504a89
goughjames45 <jamesman45@gmail.com>
2026-08-28 15:32:57 +0000

    (feat): add connect/send to hardware toolbar controls with status dot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,43 @@
 import { Component, inject } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './node-selector/node-selector.component';
 import { MainCanvasComponent } from "./main-canvas/main-canvas.component";
 import { Store } from '@ngrx/store';
 import { saveFlowchart } from './store/actions';
+import {
+  connectDevice,
+  disconnectDevice,
+  sendToHardware,
+} from './store/hardware.actions';
+import { selectDeviceStatus, selectSendStatus } from './store/selectors';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, MainCanvasComponent],
+  imports: [RouterOutlet, SidebarComponent, MainCanvasComponent, AsyncPipe, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'gooey-app';
   private store = inject(Store);
+  deviceStatus$ = this.store.select(selectDeviceStatus);
+  sendStatus$ = this.store.select(selectSendStatus);
 
   onSave() {
     this.store.dispatch(saveFlowchart({ filename: 'flowchart.json' }));
+  }
+
+  onConnect() {
+    this.store.dispatch(connectDevice());
+  }
+
+  onDisconnect() {
+    this.store.dispatch(disconnectDevice());
+  }
+
+  onSendToHardware() {
+    this.store.dispatch(sendToHardware());
   }
 }
